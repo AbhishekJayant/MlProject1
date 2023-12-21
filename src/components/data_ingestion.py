fe8fad520a2825b5058101db40d7c7ac2fe1c7ce
AbhishekJayant <abhishekjayant1111@gmail.com>
2023-12-21 23:37:56 +0000

    model trainer implemented

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,9 @@ from sklearn.model_selection import train_test_split
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     # these are the paths to store train, test and raw data files
@@ -26,9 +29,9 @@ class DataIngestion:
         try:
             df = pd.read_csv("notebook/data/stud.csv")
             logging.info("Read the csv file as dataframe")
-            os.makedirs(
-                os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True
-            )
+            # os.makedirs(
+            #     os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True
+            # )
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
             logging.info("Train-Test Split initiated")
@@ -58,3 +61,6 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
